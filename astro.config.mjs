870943e9916be4a4c4d035fc7390d37637adef32
marianocarmona <nijaraldia@gmail.com>
2026-06-21 23:29:12 +0200

    feat(seo): configure sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sendapirata.com/',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })]
+  integrations: [partytown({ config: { forward: ['dataLayer.push'] } }), sitemap()]
 });
